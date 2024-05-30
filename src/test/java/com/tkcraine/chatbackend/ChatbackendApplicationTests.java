993c9dea08ce5a7dd9174eabfffbe4638a685b69
Tyler Craine <tkcraine@gmail.com>
2024-05-30 01:30:34 +0000

    first commit

---
+++ src/test/java/com/tkcraine/chatbackend/ChatbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkcraine.chatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
